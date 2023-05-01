b63dfade90d21464f68ac2d2fb8765dc89ab00a6
Samuel Zaire <96780102+szaire@users.noreply.github.com>
2023-05-01 13:48:19 -0300

    edit some code in main.dart

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 183, 58, 116)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Meu primeiro projeto em Flutter'),
     );
   }
 }
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você apertou o botão essa quantidade de vezes:',
             ),
             Text(
               '$_counter',
